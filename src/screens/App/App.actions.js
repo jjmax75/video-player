32ea73b7b509cf71ff5bb2d66f32f7fef2d88f54
John Behan <behanj@gmail.com>
2019-04-06 16:32:39 +0100

    fix routing for json

--- src/screens/App/App.actions.js
+++ src/screens/App/App.actions.js
@@ -1,16 +1,18 @@
-import { SET_VIDEO_LIST } from "./App.constants";
+import { SET_VIDEO_LIST } from './App.constants';
 
 export const getVideoList = () => {
-	const videoList = process.env.REACT_APP_VIDEO_LIST;
+	const videoList = `${window.location.origin}${
+		process.env.REACT_APP_VIDEO_LIST
+	}`;
 	return dispatch => {
 		return fetch(videoList)
 			.then(response => response.json())
 			.then(json => dispatch(setVideoList(json)))
-			.catch(error => console.log("get video list failed:", error));
+			.catch(error => console.log('get video list failed:', error));
 	};
 };
 
 const setVideoList = videoList => ({
 	type: SET_VIDEO_LIST,
-	payload: videoList
+	payload: videoList,
 });
